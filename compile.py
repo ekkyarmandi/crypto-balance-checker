8ab6e5303b9cda98be0329ee9fc3ecb3194e5735
Ekky Armandi <ekkyarmandi@gmail.com>
2022-01-30 08:47:03 +0800

    modifying compiler compilation

--- compile.py
+++ compile.py
@@ -1,4 +1,5 @@
 import PyInstaller.__main__
+import os
 
 PyInstaller.__main__.run([
     '--name',
@@ -9,3 +10,9 @@ PyInstaller.__main__.run([
     '--icon',
     './icon/1486395296-05-wallet_80570.ico'
 ])
+
+src = "./dist/"
+dst = "./"
+file = "balance_checker.exe"
+if os.path.exists(src+file):
+    os.rename(src+file,dst+file)
--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import Chrome
 
@@ -46,9 +47,9 @@ class BalanceChecker():
 
     # open the browser
     options = Options()
-    # options.add_argument("--level-log=3")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    browser = Chrome("chromedriver.exe",options=options)
+    service = Service("chromedriver.exe")
+    browser = Chrome(service=service,options=options)
     browser.set_window_position(-10000,0)
 
     # consume the CSV file
